77e97e3d24c7f61e923eb174799249d6a72c57a9
Mohit <mohitbhoir93@gmail.com>
2023-01-12 21:00:44 +0530

    front end hackthon

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";
+import Contacts from "./pages/contacts";
+import Form from "./pages/form";
+import FAQ from "./pages/faq";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Edit from "./pages/edit";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/edit" element={<Edit/>}/>
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/faq" element={<FAQ />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
